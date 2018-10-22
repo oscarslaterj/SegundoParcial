f1df11c28735d3621c825dac3096b3b9a99d260b
Oscar Slater Jimenez Paula <oscar_jimenez@ucne.edu.do>
2018-10-22 14:12:31 -0400

    Creando entidad y contexto

---
+++ SegundoParcial/DAL/Contexto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SegundoParcial.Entidades;
+
+namespace PrimerParcial.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Vendedores> Vendedores { get; set; }
+
+        public Contexto() : base("Constr") { }
+    }
+}
---
+++ SegundoParcial/Entidades/Vendedores.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SegundoParcial.Entidades
+{
+    public class Vendedores
+    {
+        [Key]
+
+        public int VendedorId { get; set; }
+        public String Nombres { get; set; }
+        public decimal Sueldo { get; set; }
+        public decimal PorcRetencion { get; set; }
+        public decimal Retencion { get; set; }
+        public DateTime Fecha { get; set; }
+
+
+        public Vendedores()
+        {
+            VendedorId = 0;
+            Nombres = string.Empty;
+            Sueldo = 0;
+            PorcRetencion = 0;
+            Retencion = 0;
+            Fecha = DateTime.Now;
+        }
+    }
+}
